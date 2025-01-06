d85bf14ccbf319ab133ffdd3153c22bc7b22c3a7
山形駿介 <yamagatashunsuke@MacBook-Pro-2.local>
2025-01-06 23:14:47 +0900

    　スケジュール用のコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'schedules/index'
+  get 'schedules/new'
+  get 'schedules/create'
+  get 'schedules/show'
+  get 'schedules/update'
+  get 'schedules/edit'
+  get 'schedules/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
