258dbbb8ef3a3d243d90e78c707d7e25b32058d9
山形駿介 <yamagatashunsuke@MacBook-Pro-2.local>
2025-01-07 02:15:50 +0900

    不要なコメントの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-#get    'schedules/index',    to: 'schedules#index'
-#get    'schedules/new',      to: 'schedules#new'
-#post   'schedules/create',   to: 'schedules#create'
-#get    'schedules/show',     to: 'schedules#show'
-#get    'schedules/edit',     to: 'schedules#edit'
-#patch  'schedules/update',   to: 'schedules#update'
-#put    'schedules/edit',     to: 'schedules#update'
-#delete 'schedules/destroy',  to: 'schedules#destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :schedules
