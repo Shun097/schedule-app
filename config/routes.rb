d0cc3124ed6588362896cec6cdd28cd4defd96eb
山形駿介 <yamagatashunsuke@MacBook-Pro-2.local>
2025-01-06 23:53:20 +0900

    スケジュール用ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'schedules/index'
-  get 'schedules/new'
-  get 'schedules/create'
-  get 'schedules/show'
-  get 'schedules/update'
-  get 'schedules/edit'
-  get 'schedules/destroy'
+#get    'schedules/index',    to: 'schedules#index'
+#get    'schedules/new',      to: 'schedules#new'
+#post   'schedules/create',   to: 'schedules#create'
+#get    'schedules/show',     to: 'schedules#show'
+#get    'schedules/edit',     to: 'schedules#edit'
+#patch  'schedules/update',   to: 'schedules#update'
+#put    'schedules/edit',     to: 'schedules#update'
+#delete 'schedules/destroy',  to: 'schedules#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :schedules
+
 end
