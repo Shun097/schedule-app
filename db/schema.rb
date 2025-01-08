17460b150648908c58a1fb452239d6cc2aade687
山形駿介 <yamagatashunsuke@MacBook-Pro-2.local>
2025-01-08 14:29:55 +0900

    メモモデルの追加、マイグレーションの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_06_124034) do
+ActiveRecord::Schema.define(version: 2025_01_08_052748) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2025_01_06_124034) do
     t.boolean "all_day"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "memo"
   end
 
 end
